c9dd54493407862ed50c6082253e000b4c98910e
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-11-05 19:18:12 +0100

    Add custom endpoints

--- main.go
+++ main.go
@@ -2,12 +2,58 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/models"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/custom/tunes/byTuneId/:tuneId",
+			Handler: func(c echo.Context) error {
+				record, _err := app.Dao().FindFirstRecordByData("tunes", "tuneId", c.PathParam("tuneId"))
+
+				if _err != nil {
+					return c.JSON(http.StatusNotFound, _err)
+				}
+
+				_errors := app.Dao().ExpandRecord(record, []string{"author"}, func(relCollection *models.Collection, relIds []string) ([]*models.Record, error) {
+					record, _err := app.Dao().FindFirstRecordByData(relCollection.Name, "id", relIds[0])
+
+					return []*models.Record{record}, _err
+				})
+
+				if len(_errors) > 0 {
+					return c.JSON(http.StatusNotFound, _errors)
+				}
+
+				return c.JSON(http.StatusOK, record)
+			},
+		})
+
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/custom/iniFiles/bySignature/:signature",
+			Handler: func(c echo.Context) error {
+				record, _err := app.Dao().FindFirstRecordByData("iniFiles", "signature", c.PathParam("signature"))
+
+				if _err != nil {
+					return c.JSON(http.StatusNotFound, _err)
+				}
+
+				return c.JSON(http.StatusOK, record)
+			},
+		})
+
+		return nil
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
